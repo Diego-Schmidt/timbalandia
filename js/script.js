f011e8f5ae04928b570e6482a6a7190d98d281d5
Diego-Schmidt <email@diegoschmidt.com>
2022-03-15 02:30:16 -0300

    Prueba de diseño de la tabla top10 en index.html

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ const swiper = new Swiper('.swiper', {
   speed: 400,
   spaceBetween: 100,
 });
-const swiper = document.querySelector('.swiper').swiper;
+// const swiper = document.querySelector('.swiper').swiper;
 
 // Now you can use all slider methods like
 swiper.slideNext();
